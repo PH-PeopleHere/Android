14eb86ddb3080c7b5f8bdc38e262ec1d5fe45b84
cheerjihyeon <gimjihyeon111@gmail.com>
2024-02-18 22:13:33 +0900

    2024-02-18 4:47 commit

---
+++ app/src/main/java/com/peopleHere/people_here/Profile/ProfileMeActivity.java
@@ -0,0 +1,15 @@
+package com.peopleHere.people_here.Profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.peopleHere.people_here.R;
+
+public class ProfileMeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_me);
+    }
+}
